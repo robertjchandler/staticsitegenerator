4891d2825ee6ebd914e27993d0cdf9a7ac4ceb9f
robertjchandler <robertjchandler4@gmail.com>
2024-12-30 23:37:10 -0600

    update Main

--- src/main.py
+++ src/main.py
@@ -1,8 +1,27 @@
+import os
+import shutil
+
 from textnode import TextNode, TextType
 
 def main():
     node = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
     print(node)
+
+    def copy(source, destination):
+        # delete all the contents of the destination directory
+        if os.path.exists(destination):
+            shutil.rmtree(destination)
+        os.mkdir(destination)
+        os.mkdir(destination + "/images")
+        # copy all files and subdirectories
+        contents = os.listdir(source)
+        for file in contents:
+            shutil.copy(file, destination)
+
+    root = "/home/robert_chandler/workspace/staticsitegenerator/"
+    source = root + "static"
+    destination = root + "public"
+    copy(source, destination)
 
 main()
 
